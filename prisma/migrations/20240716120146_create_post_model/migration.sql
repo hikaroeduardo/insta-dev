10a929ec4f43bdefe81e16e176bbd12b64604ab9
Hikaro Eduardo <hikaroeduardobandeira@gmail.com>
2024-07-16 09:02:10 -0300

    feat: add post model

---
+++ prisma/migrations/20240716120146_create_post_model/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "posts" (
+    "id" SERIAL NOT NULL,
+    "image" TEXT NOT NULL,
+    "description" TEXT,
+    "number_value" INTEGER NOT NULL DEFAULT 0,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "author_id" INTEGER NOT NULL,
+
+    CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
